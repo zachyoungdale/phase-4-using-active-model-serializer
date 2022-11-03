7882f1c731a6da111f0232bf6ffad999641049e6
Zachary Youngdale <zyoungdale@gmail.com>
2022-11-03 14:04:24 -0400

    commit tree 6bb3e85d19feb8dca28c5e6eb7b1b84829ebdee6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,16 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summary 
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+
   private
 
   def render_not_found_response
---
+++ app/serializers/movie_serializer.rb
@@ -0,0 +1,6 @@
+class MovieSerializer < ActiveModel::Serializer
+  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director
+
+
+  
+end
